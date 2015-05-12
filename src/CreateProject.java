f95cdc3086cf0cca0fa84ba037e6144a733ca66a
tom wang <tong.wang@liferay.com>
2015-05-12 09:59:51 -0700

    formatting

--- src/CreateProject.java
+++ src/CreateProject.java
@@ -15,49 +15,67 @@ import org.w3c.dom.Element;
 public class CreateProject {
 	public static void createConfiguration(Element projectElement) {
 		Element configurationElement = document.createElement("configuration");
+
 		projectElement.appendChild(configurationElement);
 
 		createData(configurationElement);
+
 		createLibraries(configurationElement);
 	}
 
 	public static void createData(Element configurationElement) {
 		Element dataElement = document.createElement("data");
+
 		dataElement.setAttribute("xmlns", "http://www.netbeans.org/ns/j2se-project/3");
+
 		configurationElement.appendChild(dataElement);
 
 		Element nameElement = document.createElement("name");
+
 		nameElement.appendChild(document.createTextNode(_projectName));
+
 		dataElement.appendChild(nameElement);
 
 		Element sourceRootsElement = document.createElement("source-roots");
+
 		dataElement.appendChild(sourceRootsElement);
+
 		for(String module:_modules) {
 			createRoots(sourceRootsElement,"src."+module+".dir");
 		}
 
 		Element testRootsElement = document.createElement("test-roots");
+
 		dataElement.appendChild(testRootsElement);
+
 		createRoots(testRootsElement, "test.src.dir");
 	}
 
 	public static void createLibraries(Element configurationElement) {
 		Element librariesElement = document.createElement("libraries");
+
 		librariesElement.setAttribute("xmlns", "http://www.netbeans.org/ns/ant-project-libraries/1");
+
 		configurationElement.appendChild(librariesElement);
 
 		Element definitionsElement = document.createElement("definitions");
+
 		definitionsElement.appendChild(document.createTextNode("./lib/nblibraries.properties"));
+
 		librariesElement.appendChild(definitionsElement);
 	}
 
 	public static void createProjectElement() {
 		Element projectElement = document.createElement("project");
+
 		projectElement.setAttribute("xmlns", "http://www.netbeans.org/ns/project/1");
+
 		document.appendChild(projectElement);
 
 		Element typeElement = document.createElement("type");
+
 		typeElement.appendChild(document.createTextNode("org.netbeans.modules.java.j2seproject"));
+
 		projectElement.appendChild(typeElement);
 
 		createConfiguration(projectElement);
@@ -65,7 +83,9 @@ public class CreateProject {
 
 	public static void createRoots(Element sourceRootsElement, String module) {
 		Element rootElement = document.createElement("root");
+
 		rootElement.setAttribute("id", module);
+
 		sourceRootsElement.appendChild(rootElement);
 	}
 	public static void main(String[] args) throws Exception {
@@ -107,8 +127,7 @@ public class CreateProject {
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println(
 				"Insufficient number of inputs, please use the following " +
-				"order of inputs: GroupId, ArtifactId, Version, Packaging, " +
-				"Name, FullPath-to-module, Modules, Dependencies");
+				"order of inputs: Project Name, module list");
 
 			System.exit(1);
 		}
