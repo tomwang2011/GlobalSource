40e48a64a37a9dfc5fff4544c43b304b5e605759
tomwang2011 <tong.wang@liferay.com>
2015-07-30 17:11:20 -0700

    Moved source to same package

--- src/com/liferay/netbeansproject/AppendLibJars.java
+++ src/com/liferay/netbeansproject/AppendLibJars.java
@@ -1,3 +1,4 @@
+package com.liferay.netbeansproject;
 
 import java.io.BufferedWriter;
 import java.io.File;
--- src/com/liferay/netbeansproject/CreateProject.java
+++ src/com/liferay/netbeansproject/CreateProject.java
@@ -1,3 +1,5 @@
+package com.liferay.netbeansproject;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
