210fafc8c55c3be87c1fe503b3a1c4afe9b8d195
tomwang2011 <tong.wang@liferay.com>
2015-07-30 17:28:00 -0700

    Use correct exceptions

--- src/com/liferay/netbeansproject/AppendLibJars.java
+++ src/com/liferay/netbeansproject/AppendLibJars.java
@@ -2,46 +2,46 @@ package com.liferay.netbeansproject;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class AppendLibJars {
 
-	public static void appendJars(String jarList) {
+	public static void appendJars(String jarList) throws FileNotFoundException {
 		String[] jars = jarList.split(File.pathSeparator);
 
 		File propertyFile = new File("portal/nbproject/project.properties");
 
-		if (propertyFile.exists()) {
-			try (
-				PrintWriter printWriter = new PrintWriter(
-					new BufferedWriter(new FileWriter(propertyFile, true)))) {
+		if (!propertyFile.exists()) {
+			throw new FileNotFoundException("Project.properties not found");
+		}
+		try (
+			PrintWriter printWriter = new PrintWriter(
+				new BufferedWriter(new FileWriter(propertyFile, true)))) {
 
-				for (String jarPath : jars) {
-					String[] jarPathSplit = jarPath.split("/");
+			for (String jarPath : jars) {
+				String[] jarPathSplit = jarPath.split("/");
 
-					String jar = jarPathSplit[jarPathSplit.length - 1];
+				String jar = jarPathSplit[jarPathSplit.length - 1];
 
-					printWriter.println(
-						"file.reference." + jar + "=" + jarPath);
-				}
-
-				printWriter.println("javac.classpath=\\");
-
-				for (String jarPath : jars) {
-					String[] jarPathSplit = jarPath.split("/");
-
-					String jar = jarPathSplit[jarPathSplit.length - 1];
-
-					printWriter.println("${file.reference." + jar + "}:\\");
-				}
+				printWriter.println(
+					"file.reference." + jar + "=" + jarPath);
 			}
-			catch (Exception e) {
-				e.printStackTrace();
+
+			printWriter.println("javac.classpath=\\");
+
+			for (String jarPath : jars) {
+				String[] jarPathSplit = jarPath.split("/");
+
+				String jar = jarPathSplit[jarPathSplit.length - 1];
+
+				printWriter.println("${file.reference." + jar + "}:\\");
 			}
 		}
-		else {
-			System.out.println("file not found");
+		catch (IOException e) {
+			System.out.println(e.getClass());
 		}
 	}
 
