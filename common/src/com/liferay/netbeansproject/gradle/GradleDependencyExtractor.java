4c47f59221b48346bbd3a5afebeee32870200621
shuyangzhou <shuyang.zhou@liferay.com>
2015-10-28 19:48:00 -0700

    Just directly use Path

--- common/src/com/liferay/netbeansproject/gradle/GradleDependencyExtractor.java
+++ common/src/com/liferay/netbeansproject/gradle/GradleDependencyExtractor.java
@@ -1,24 +1,23 @@
 package com.liferay.netbeansproject.gradle;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class GradleDependencyExtractor {
-	public static void main(String[] args) throws IOException {
-		File gradleDependencyFile = new File("GradleDependency.properties");
 
+	public static void main(String[] args) throws IOException {
 		StringBuilder sb = new StringBuilder("compile=");
 
 		sb.append(args[0]);
-		sb.append("\n");
-		sb.append("compileTest=");
+		sb.append("\ncompileTest=");
 		sb.append(args[1]);
 
 		Files.write(
-			gradleDependencyFile.toPath(), Arrays.asList(sb),
+			Paths.get("GradleDependency.properties"), Arrays.asList(sb),
 			Charset.defaultCharset());
 	}
+
 }
