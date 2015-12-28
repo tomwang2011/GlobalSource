dd1163a4f6f22c58f63ed3337c6e9a969c168b9c
shuyangzhou <shuyang.zhou@liferay.com>
2015-12-28 09:59:27 -0800

    Use ArgumentsUtil

--- java-application-module-project/src/com/liferay/netbeansproject/ProcessGradle.java
+++ java-application-module-project/src/com/liferay/netbeansproject/ProcessGradle.java
@@ -1,19 +1,23 @@
 package com.liferay.netbeansproject;
 
+import com.liferay.netbeansproject.util.ArgumentsUtil;
 import java.io.InterruptedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author tom
  */
 public class ProcessGradle {
 	public static void main(String[] args) throws Exception {
-		Path portalDirPath = Paths.get(args[0]);
-		Path projectFilePath = Paths.get(args[1]);
+		Map<String, String> arguments = ArgumentsUtil.parseArguments(args);
+
+		Path portalDirPath = Paths.get(arguments.get("portal.dir"));
+		Path projectFilePath = Paths.get(arguments.get("project.dir"));
 
 		Path gradlewPath = portalDirPath.resolve("gradlew");
 
