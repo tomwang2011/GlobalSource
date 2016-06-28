15a95d8c7a7849f5720e1a9c5e73ec66c22cc219
tomwang2011 <tong.wang@liferay.com>
2016-06-28 14:23:25 -0700

    Override equals and hashCode methods for Dependency

--- src/com/liferay/netbeansproject/container/Dependency.java
+++ src/com/liferay/netbeansproject/container/Dependency.java
@@ -14,7 +14,10 @@
 
 package com.liferay.netbeansproject.container;
 
+
 import java.nio.file.Path;
+
+import java.util.Objects;
 
 /**
  * @author Tom Wang
@@ -26,8 +29,36 @@ public class Dependency {
 		_test = test;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (!(obj instanceof Dependency)) {
+			return false;
+		}
+
+		Dependency dependency = (Dependency)obj;
+
+		if (Objects.equals(_path, dependency._path)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public Path getName() {
+		return _path.getFileName();
+	}
+
 	public Path getPath() {
 		return _path;
+	}
+
+	@Override
+	public int hashCode() {
+		return _path.hashCode();
 	}
 
 	public boolean isTest() {
